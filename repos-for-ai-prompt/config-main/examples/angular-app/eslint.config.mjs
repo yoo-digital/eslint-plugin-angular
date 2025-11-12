3e2ec76e1f15d2696598ce0a48a00a11a15648ff
Sylvain Hugues YOO <sylvain.hugues@yoo.digital>
2025-11-12 20:20:17 +0100

    more

--- repos-for-ai-prompt/config-main/examples/angular-app/eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-// @ts-check
-import eslintBaseConfig from '@yoo-digital/eslint-config-base';
-import eslintAngularConfig from '@yoo-digital/eslint-config-angular';
-
-export default [...eslintBaseConfig, ...eslintAngularConfig];
--- repos-for-ai-prompt/config-main/examples/react-app/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import yooEslintConfigReact from '@yoo-digital/eslint-config-react';
-import reactRefresh from 'eslint-plugin-react-refresh';
-
-export default [...yooEslintConfigReact, reactRefresh.configs.vite];
